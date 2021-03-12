1738ff0af59ada2ac80c77494d28f0f2b39e302a
DimitriNakos <dimi1nakos@gmail.com>
2021-03-12 18:38:20 -0500

    tinkering with some things and adding styling

--- day_01/day01.js
+++ day_01/day01.js
@@ -7,34 +7,55 @@ const ride = function(){document.getElementById("06").play()}
 const snare = function(){document.getElementById("07").play()}
 const tink = function(){document.getElementById("08").play()}
 
-window.addEventListener("keydown", (e) => {
+const btn0 = document.querySelector(".btn0");
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".btn2");
+const btn3 = document.querySelector(".btn3");
+const btn4 = document.querySelector(".btn4");
+const btn5 = document.querySelector(".btn5");
+const btn6 = document.querySelector(".btn6");
+const btn7 = document.querySelector(".btn7");
+
+const hoverMimic = (x) => {
+    x.classList.add("pressed")
+    setTimeout(() => {
+        x.classList.remove("pressed")
+    }, 500)
+}
+
+window.addEventListener("keypress", (e) => {
     switch (e.key) {
         case "a":
+            hoverMimic(btn0);
             boom();
             break;
         case "s":
+            hoverMimic(btn1);
             clap();
             break;
         case "d":
+            hoverMimic(btn2)
             hiHat();
             break;
         case "f":
+            hoverMimic(btn3)
             kick();
             break;
         case "g":
+            hoverMimic(btn4)
             openHat();
             break;
         case "h":
+            hoverMimic(btn5)
             ride();
             break;
         case "j":
+            hoverMimic(btn6)
             snare();
             break;
         case "k":
+            hoverMimic(btn7)
             tink();
             break;
     }
-
 })
-
-
