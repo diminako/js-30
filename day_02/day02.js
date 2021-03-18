708420c947941be879cc0d0b470762b6e45f3b9b
DimitriNakos <dimi1nakos@gmail.com>
2021-03-18 10:10:11 -0400

    JS CSS clock refactored

--- day_02/day02.js
+++ day_02/day02.js
@@ -2,21 +2,39 @@ const secondHand = document.querySelector(".second-hand");
 const minHand = document.querySelector(".min-hand");
 const hourHand = document.querySelector(".hour-hand");
 
-const setDate = () => {
-    
-    const now = new Date();
-    const seconds = now.getSeconds();
+function secSet(x) {
+    const seconds = x.getSeconds();
+    if (seconds == 0) {
+        secondHand.style.transition = "none";
+    }
     const secondsDegrees = ((seconds/60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`
-    
-    const minutes = now.getMinutes();
+}
+
+function minSet(x) {
+    const minutes = x.getMinutes();
+    if (minutes == 0) {
+        minHand.style.transition = "none";
+    }
     const minDegrees = ((minutes/60) * 360) + 90;
     minHand.style.transform = `rotate(${minDegrees}deg)`
-    
-    const hours = now.getHours();
-    const hourDegrees = ((hours/60) * 360) + 90;
-    hourHand.style.transform = `rotate(${hourDegrees}deg)`
+}
 
+function hourSet(x) {
+    const hours = x.getHours();
+    if (hours == 12) {
+        hourHand.style.transition = "none";
+    }
+    const hourDegrees = ((hours/12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`
+}
+
+const setDate = () => {
+    const now = new Date();
+
+    secSet(now);
+    minSet(now);
+    hourSet(now);
 }
 
 setInterval(setDate, 1000);
